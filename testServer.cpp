c48be164df5f2a3d63ff56770e5d582ec6bea950
wtraceyv <wtraceyv@gmail.com>
2019-03-18 23:28:40 -0400

    Update testServer.cpp

--- testServer.cpp
+++ testServer.cpp
@@ -1,1 +1,10 @@
-
+/**
+ * There will be the barebones of a client here 
+ * that will run as long as we need it to and 
+ * allow anyone to access the functions of the 
+ * site/app online from anywhere as long as 
+ * they can get the right port number.
+ *
+ * Primarily just for testing the app's 
+ * to live online like a website/browser app.
+ */ 

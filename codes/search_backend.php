f25c7413b116284f31d492e7dcbc406dca79a6a9
Ruoxin Li <lir25@miamioh.edu>
2019-04-09 13:47:04 -0400

    Add the logo on the left of tab title

--- codes/search_backend.php
+++ codes/search_backend.php
@@ -25,7 +25,7 @@ $searchQuery = $_SESSION['searchQuery'];
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
    <link rel="stylesheet" href="../css/mainCSS.css">
-
+   <link rel="shortcut icon" href="../images/logo.ico"> 
    <title>Search Result</title>
 </head>
 <body>
